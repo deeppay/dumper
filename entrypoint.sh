8c2c222d9498b2e92496b86246dca0a5c3976c76
Tomasz Pichlak <tpichlak@gmail.com>
2017-10-24 13:17:53 +0200

    Moved entrypoint.sh to etc added logback_deployment.xml and edited dockerfile.

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar /root/dumper.jar --db=$Cassandra_Address --keyspace=$Cassandra_Keyspace --kafka=$Kafka_Broker --prefix=$Prefix --user=$User --pass=$Password  --statSDHost=$StatSD_Host
