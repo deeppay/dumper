e1cc9b4001c39f5b7013b876a9e3ee3731af968e
Tomasz Pichlak <tpichlak@gmail.com>
2017-10-31 10:35:20 +0100

    Addded insertinf and deleting records from real_time_jobs table.

--- src/main/scala/carldata/dumper/RealTimeProcessor.scala
+++ src/main/scala/carldata/dumper/RealTimeProcessor.scala
@@ -1,12 +1,14 @@
 package carldata.dumper
 
 import carldata.hs.RealTime.RealTimeJsonProtocol._
-import carldata.hs.RealTime.RealTimeJobRecord
-import com.datastax.driver.core.querybuilder.{Clause, QueryBuilder}
+import carldata.hs.RealTime.{AddAction, ErrorAction, RealTimeJobRecord, RemoveAction}
+import com.datastax.driver.core.querybuilder.QueryBuilder
 import com.datastax.driver.core.{BatchStatement, Statement}
 import org.slf4j.LoggerFactory
 import spray.json.JsonParser
 import spray.json.JsonParser.ParsingException
+
+import scala.collection.JavaConverters._
 
 object RealTimeProcessor {
 
@@ -15,10 +17,30 @@ object RealTimeProcessor {
   private val Log = LoggerFactory.getLogger(this.getClass)
 
   def process(messages: Seq[String]): Option[Statement] = {
-
-    QueryBuilder.delete().from(TABLE_NAME).where(QueryBuilder.eq("","")).and(QueryBuilder.eq("",""))
-      .and(QueryBuilder.eq("",""))
-    None
+    val records = messages.flatMap(deserialize)
+    if (records.isEmpty) None
+    else {
+      val batch = new BatchStatement()
+      records.foreach(r => {
+        if (r.action == AddAction) {
+          println("Adding Job")
+          val addJob = QueryBuilder.insertInto(TABLE_NAME)
+            .value("calculation", r.calculationId)
+            .value("script", r.script)
+            .value("input_channels", r.inputChannelIds.asJava)
+            .value("output_channel", r.outputChannelId)
+          batch.add(addJob)
+        } else if (r.action == RemoveAction) {
+          println("Removing job")
+          val removeJob = QueryBuilder.delete().from(TABLE_NAME)
+            .where(QueryBuilder.eq("calculation", r.calculationId))
+          batch.add(removeJob)
+        } else if (r.action == ErrorAction) {
+          Log.error("Error action occurred: " + r.toString)
+        }
+      })
+      Some(batch)
+    }
   }
 
   def deserialize(job: String): Option[RealTimeJobRecord] = {
